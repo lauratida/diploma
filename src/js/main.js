d74c7d0f9bc6e08646a3695aae7eff68aafd5e24
Nataly <html-tida@mail.ru>
2019-08-23 13:57:26 +0300

    Секция акции месяца. Небольшие правки от куратора

--- src/js/main.js
+++ src/js/main.js
@@ -29,4 +29,22 @@ $(document).ready(function () {
   // Добавляем классы для стилизации созданным слайдером элементам
   $('.slick-dots').children().addClass('slick-dots__item');
   $(".slick-dots").wrap("<div class='container container-slider'></div>");
+
+  $('.stock-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    fade: true,
+    dotsClass: 'stock-slider-dot',
+    customPaging: function(slick, index) {
+      var image = $(slick.$slides[index]).find('.slider__img').attr('srcprev');
+      return '<img src="' + image + '" alt="" /> '
+    },
+    appendDots: $('.stock-slider__dots'),
+    customPaging : function(slider, i) {
+        var thumb = $(slider.$slides[i]).data();
+        return '<a>'+(i+1).toString().padStart(2, '0')+'</a>';
+    },
+  });
 });
